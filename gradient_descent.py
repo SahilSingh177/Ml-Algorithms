aefb66f3ec97e072e4f1e8901836b97af84584ac
Sahil <lcs2021033@iiitl.ac.in>
2023-10-27 18:09:26 +0530

    Added visualisation for gradient descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,6 +1,8 @@
 import sympy
 from typing import Any
-
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class GradientDescent:
     def __init__(self, func: Any, start_point: int, end_point: int):
@@ -14,8 +16,9 @@ class GradientDescent:
         # Set learning factor
         alpha = 0.1
         # Set maxsteps
-        max_steps = 10000
-
+        max_steps = 1000
+        # Storing the results in a variable for plotting
+        history = []
         a = self.start_point
         b = self.end_point
         # Loop till you find min or maxSteps is exhausted
@@ -28,12 +31,28 @@ class GradientDescent:
             a = a - (x_deriv*alpha)
             b = b - (y_deriv*alpha)
             step += 1
+            history.append((a,b,self.func.subs({x:a,y:b})))
         print(a, b)
-
+        return a,b,self.func,history
 
 x = sympy.Symbol('x')
 y = sympy.Symbol('y')
 a = 1  # Starting x-coordinate
 b = 1  # Starting y-coordinate
 a = GradientDescent((y+1)**2+(x)**2, a, b)
-a.findMin()
+x_opt,y_opt,f_opt,history = a.findMin()
+
+# Plotting
+x_range = np.arange(-10,10,0.1)
+y_range = np.arange(-10,10,0.1)
+X,Y = np.meshgrid(x_range,y_range)
+Z = (Y+1)**2+(X)**2
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='coolwarm')
+ax.scatter(*zip(*history), c='r', marker='o')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('f(x, y)')
+plt.show()
